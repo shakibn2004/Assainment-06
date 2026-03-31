ea30a4d40f45e7888b467b6b2a8ca8ac6d885646
shakibn2004 <shakibn2004@gmail.com>
2026-03-31 19:08:01 +0600

    File path Problems solved

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -61,7 +61,7 @@ const Navbar = ({ selectedProduct, setRenderCard }) => {
                   !selectedProduct.length ? "hidden" : "flex"
                 } items-center justify-center w-4 h-4 cursor-pointer bg-red-500 rounded-full absolute -top-1.25 left-2.5`}
               >
-                <p className="text-white text-center text-sm">
+                <p className="text-white text-center text-sm cursor-pointer">
                   {selectedProduct.length}
                 </p>
               </div>
